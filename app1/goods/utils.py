63e89528b461c376a52cbfdbbd1fa58c5bf4c9da
Andrey Gordeev <multiistomin@gmail.com>
2025-08-01 16:45:50 +0300

    add

--- app1/goods/utils.py
+++ app1/goods/utils.py
@@ -3,7 +3,7 @@ from django.contrib.postgres.search import (
     SearchVector,
     SearchQuery,
     SearchRank,
-    # SearchHeadline,
+    SearchHeadline,
 )
 
 from goods.models import Products
@@ -15,29 +15,30 @@ def q_search(query):
 
     vector = SearchVector("name", "description")
     query = SearchQuery(query)
-    # Entry.objects.annotate(rank=SearchRank(vector, query)).order_by("-rank")
-    return Products.objects.annotate(rank=SearchRank(vector, query)).order_by("-rank")
-    # result = (
-    #     Products.objects.annotate(rank=SearchRank(vector, query))
-    #     .filter(rank__gt=0)
-    #     .order_by("-rank")
-    # )
+    
+    # return Products.objects.annotate(rank=SearchRank(vector, query)).order_by("-rank")
+    
+    result = (
+        Products.objects.annotate(rank=SearchRank(vector, query))
+        .filter(rank__gt=0)
+        .order_by("-rank")
+    )
 
-    # result = (
-    #     Products.objects.annotate(rank=SearchHeadline(vector, query))
-    #     .filter(rank__gt=0)
-    #     .order_by("-rank")
-    # )
+    result = (
+        Products.objects.annotate(rank=SearchHeadline(vector, query))
+        .filter(rank__gt=0)
+        .order_by("-rank")
+    )
 
-    # result = result.annotate(
-    #     headline=SearchHeadline(
-    #         "name",
-    #         query,
-    #         start_sel='<span style="background-color: yellow;">',
-    #         stop_sel="</span>",
-    #     )
-    # )
-    # return result
+    result = result.annotate(
+        headline=SearchHeadline(
+            "name",
+            query,
+            start_sel='<span style="background-color: yellow;">',
+            stop_sel="</span>",
+        )
+    )
+    return result
     
     
     # keywords = [word for word in query.split() if len(word) > 2]
