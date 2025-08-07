d26765b734475d40c185ecbe02006f8034dfbcb1
Andrey Gordeev <multiistomin@gmail.com>
2025-08-07 15:34:03 +0300

    add form registration

---
+++ app1/users/urls.py
@@ -0,0 +1,18 @@
+# это созданный мной urls.py для моих нужд
+
+from django.contrib import admin
+from django.urls import path
+
+from users import views
+# прописываем тут путь к страничкам
+
+app_name = 'users'
+
+urlpatterns = [
+    path('login/', views.login, name='login'),
+    path('registration/', views.registration, name='registration'),
+    path('profile/', views.profile, name='profile'),
+    path('logout/', views.logout, name='logout'),
+    
+
+]
