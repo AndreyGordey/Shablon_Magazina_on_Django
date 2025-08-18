3b766ab536e0ce6c8c730e9e45a12f64146ea181
Andrey Gordeev <multiistomin@gmail.com>
2025-08-18 16:25:06 +0300

    add

--- app1/users/forms.py
+++ app1/users/forms.py
@@ -7,11 +7,14 @@ from users.models import User
 
 class UserLoginForm(forms.ModelForm):
     
-    username = forms.CharField(widget=forms.TextInput(attrs={"autofocus": True, 
+    username = forms.CharField(
+        label= 'Имя',
+        widget=forms.TextInput(attrs={"autofocus": True, 
                                                              'class': 'form-control',
                                                              'placeholder': 'Введите имя пользователя' })
     )
     password = forms.CharField(
+        label= 'Пароль',
         widget=forms.PasswordInput(attrs={"autocomplete": "current-password",
                                           'class': 'form-control',
                                           'placeholder': 'Введите ваш пароль'}),
